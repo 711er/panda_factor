bc94a2c1082e050a933d1c239f1282b026de6cc6
711er <bruceh1908@gmail.com>
2025-12-10 16:21:16 +0800

    Add notebooks directory for scratching which is ignored Add panda_server and panda_web entry points in setup.py Add web_service config

--- setup.py
+++ setup.py
@@ -2,29 +2,31 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name='panda_data',
-    version='0.1',
+    name="panda_data",
+    version="0.1",
     packages=find_packages(),  # 这会自动找到所有包
     include_package_data=True,
     package_data={
-        'panda_data': [
-            'factor/**/*',
-            'market_data/**/*',
-            'scripts/**/*',
-            '*.yaml',
+        "panda_data": [
+            "factor/**/*",
+            "market_data/**/*",
+            "scripts/**/*",
+            "*.yaml",
         ],
     },
     install_requires=[
-        'flask',
-        'pymongo',
-        'redis',
-        'loguru',
-        'panda_common',
+        "flask",
+        "pymongo",
+        "redis",
+        "loguru",
+        "panda_common",
         # 'ta-lib',  # Temporarily commented out
     ],
     entry_points={
-        'console_scripts': [
-            'panda_data = panda_data.__main__:main',
+        "console_scripts": [
+            "panda_data = panda_data.__main__:main",
+            "panda_server = panda_server.__main__:main",
+            "panda_web = panda_web.__main__:main",
         ],
     },
-) 
+)
